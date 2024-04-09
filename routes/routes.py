a108b857e9a1b4a9722794ed633642ef71956c4e
blondfrogs <8285518+blondfrogs@users.noreply.github.com>
2024-04-09 14:10:33 -0600

    Add more data to api responses

--- routes/routes.py
+++ routes/routes.py
@@ -16,6 +16,20 @@ import time, datetime
 # Get the logger so we can log
 logger = setup_logger()
 
+ANSIBLE_RETURN_CODES = {
+    0: "The command ran successfully, without any task failures or internal errors.",
+    1: "There was a fatal error or exception during execution.",
+    3: "Hosts unreachable",
+    4: "Hosts unreachable",
+    5: "Error with the options provided to the command",
+    6: "Command line args are not UTF-8 encoded",
+    8: "A condition called RUN_FAILED_BREAK_PLAY occurred within Task Queue Manager",
+    99: "Ansible received a keyboard interrupt (SIGINT) while running the playbook- i.e. the user hits Ctrl+c during the playbook run",
+    143: "Ansible received a kill signal (SIGKILL) during the playbook run- i.e. an outside process kills the ansible-playbook command.",
+    250: "Unexpected exception- often due to a bug in a module, jinja templating errors, etc. (ref1)",
+    255: "Unknown error, per TQM"
+}
+
 # Default call
 def base():
     return jsonify({"message": "This is a whitelisted API endpoint with API key authentication"}), 200
@@ -111,6 +125,9 @@ def checkstatus():
     # If we have the command, report our status
     if not command.is_null():
         if command.tracker_event.is_set():
+            rc_message = 'default message'
+            if command.result.rc in ANSIBLE_RETURN_CODES:
+                rc_message = ANSIBLE_RETURN_CODES.get(command.result.rc)
             return jsonify(
                 {
                     "status": "completed",
@@ -118,14 +135,18 @@ def checkstatus():
                     "ansible_completed_time": timestamp_to_datestring(command.completed_timestamp),
                     "tag": command.tag,
                     "pattern": command.pattern,
-                    "result": command.result.output
+                    "result": command.result.output,
+                    "ansible_return_code": command.result.rc,
+                    "ansible_return_code_message": rc_message
                 }
             )
         else:
             return jsonify(
                 {
                     "status": "running",
-                    "ansible_started_time": timestamp_to_datestring(command.started_timestamp)
+                    "ansible_started_time": timestamp_to_datestring(command.started_timestamp),
+                    "tag": command.tag,
+                    "pattern": command.pattern,
                 }
             )
     else:

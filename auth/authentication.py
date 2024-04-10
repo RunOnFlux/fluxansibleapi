5a9f6ff9aab815d13264ad71a771faab6b40a9a3
blondfrogs <8285518+blondfrogs@users.noreply.github.com>
2024-04-10 10:17:06 -0600

    Make sure we check IP addresses belong to the api-key

--- auth/authentication.py
+++ auth/authentication.py
@@ -10,22 +10,23 @@ from logger.logs import setup_logger
 
 logger = setup_logger()
 
-# Define a dictionary of API keys and their corresponding IP addresses
-api_keys = API_KEYS
-
 def authenticate_request():
 
     # Get the client's IP address
     client_ip = request.remote_addr
 
     logger.info('Received Request from: %s', str(client_ip))
-    
-    # Check if the client's IP address is in the allowed list for any API key
-    authorized_ips = set(api_keys.values())
-    if client_ip not in authorized_ips:
-        return jsonify({"error": "Unauthorized IP address"}), 401
-    
+
     # Check if the request contains a valid API key
     api_key = request.headers.get('X-API-Key')
-    if api_key not in api_keys:
+    if api_key not in API_KEYS:
+        logger.info('Request Denied: Invalid API key from: %s', str(client_ip))
         return jsonify({"error": "Invalid API key"}), 401
+    
+    # Check if the client's IP address is in the allowed list for specific api key
+    authorized_ips = set(API_KEYS.get(api_key))
+    if client_ip not in authorized_ips:
+        logger.info('Request Denied: Unauthorized IP address from: %s', str(client_ip))
+        return jsonify({"error": "Unauthorized IP address"}), 401
+    
+    

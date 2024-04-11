a42350bfaa1a004501cc6c35e0136b604f4de37a
David White <david@runonflux.io>
2024-04-11 10:14:00 +0100

    First commit

--- app.py
+++ app.py
@@ -3,15 +3,17 @@
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or https://www.opensource.org/licenses/mit-license.php.
 
+import atexit
+import os
+
 from flask import Flask
 from flask_sslify import SSLify
-from auth.authentication import authenticate_request, setup_limiter
-from thread_tracker.tracker import schedule_start, cleanup
-from routes.routes import sendcommand, checkstatus, base
-from logger.logs import setup_logger
 
-import os
-import atexit
+from auth.authentication import authenticate_request, setup_limiter
+from logger.logs import setup_logger
+from routes.routes import base, checkstatus, sendcommand
+from thread_tracker.tracker import cleanup, schedule_start
+
 
 # Graceful shutdown hook for Gunicorn
 def on_exit(server):
--- auth/authentication.py
+++ auth/authentication.py
@@ -4,45 +4,48 @@
 # file LICENSE or https://www.opensource.org/licenses/mit-license.php.
 
 
-from flask import request, jsonify
-from config.config import API_KEYS
-from logger.logs import setup_logger
+from flask import Flask, Response, jsonify, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from redis import Redis
+
+from config.config import API_KEYS
+from logger.logs import setup_logger
 
 logger = setup_logger()
 
-def authenticate_request():
+
+def authenticate_request() -> Response:
 
     # Get the client's IP address
     client_ip = request.remote_addr
 
-    logger.info('Received Request from: %s', str(client_ip))
+    logger.info(f"Received Request from: {client_ip}")
 
     # Check if the request contains a valid API key
-    api_key = request.headers.get('X-API-Key')
+    api_key = request.headers.get("X-API-Key")
     if api_key not in API_KEYS:
-        logger.info('Request Denied: Invalid API key from: %s', str(client_ip))
+        logger.info(f"Request Denied: Invalid API key from: {client_ip}")
         return jsonify({"error": "Invalid API key"}), 401
-    
+
     # Check if the client's IP address is in the allowed list for specific api key
     authorized_ips = set(API_KEYS.get(api_key))
     if client_ip not in authorized_ips:
-        logger.info('Request Denied: Unauthorized IP address from: %s', str(client_ip))
+        logger.info(
+            f"Request Denied: Unauthorized IP address from: {client_ip}"
+        )
         return jsonify({"error": "Unauthorized IP address"}), 401
-    
-    
-def setup_limiter(theapp, is_prod):
-    memory_location = 'memory://'
 
-    if (is_prod):
+
+def setup_limiter(theapp: Flask, is_prod: bool) -> Limiter:
+    memory_location = "memory://"
+
+    if is_prod:
         # Redis connection URI
-        memory_location = theapp.config['REDIS_SERVER_PROD'] 
-    
-    return  Limiter(
+        memory_location = theapp.config["REDIS_SERVER_PROD"]
+
+    return Limiter(
         get_remote_address,
         app=theapp,
         default_limits=["1000 per day", "100 per hour", "10 per minute"],
-        storage_uri=memory_location
+        storage_uri=memory_location,
     )
